57c931e45c59c4a61234d9b59ffb65cd96dfabf9
myaser <me.MahmoudYaser@gmail.com>
2013-08-21 18:09:01 +0200

    fixing tests

--- corpus_browser/scrapper/tests/test_crawler.py
+++ corpus_browser/scrapper/tests/test_crawler.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from scrapper import models, crawler, tasks
 
+
 class DefaultTest(TestCase):
     def setup(self):
-        models.Criterion.create(type='hash_tag', value=u'مصر')
-        models.Criterion.create(type='hash_tag', value=u'وطن')
-        models.Criterion.create(type='hash_tag', value=u'سيناء')
-        models.Criterion.create(type='hash_tag', value=u'عرب')
-        models.Criterion.create(type='user_name', value=u'moemenology')
-        models.Criterion.create(type='user_name', value=u'mah_yaser')
+        models.Criterion.objects.create(type='hash_tag', value=u'مصر')
+        models.Criterion.objects.create(type='hash_tag', value=u'وطن')
+        models.Criterion.objects.create(type='hash_tag', value=u'سيناء')
+        models.Criterion.objects.create(type='hash_tag', value=u'عرب')
+        models.Criterion.objects.create(type='user_name', value=u'moemenology')
+        models.Criterion.objects.create(type='user_name', value=u'Mah_Yaser')
 
 
 class FetchTest(DefaultTest):
