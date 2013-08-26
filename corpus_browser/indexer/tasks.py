b9b73a137bc2d5ed6705d6d70b66504926cc20b3
myaser <me.MahmoudYaser@gmail.com>
2013-08-26 06:46:30 +0200

    bug fixing mongoengine

--- corpus_browser/indexer/tasks.py
+++ corpus_browser/indexer/tasks.py
@@ -16,11 +16,14 @@ def build_index():
     indexed_tweets = Tweet.objects.index(created_at__gte=yesterday)
 
     for token, postings in indexed_tweets.items():
-        postings_list = [Posting(document_id=doc_id, positions=pos)
+        postings_list = [Posting(document=Tweet.objects.get(id=doc_id), positions=pos)
                          for doc_id, pos in postings.items()]
+#         import pdb; pdb.set_trace()
         index_entery = AuxiliaryIndex.objects.get_or_create(token=token)[0]
-        index_entery.postings += postings_list
-        index_entery.save()
+        AuxiliaryIndex.objects(id=index_entery.id).update_one(
+                                              push_all__postings=postings_list)
+#         index_entery.postings += postings_list
+#         index_entery.save()
 
 
 @periodic_task(run_every=crontab(day_of_week="6", hour="13"), enabled=True)
