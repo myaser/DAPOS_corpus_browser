4cc9e86845cc210a6c09ed1a3dea4e917c97f633
moemen <moemenology@gmail.com>
2013-09-02 16:34:06 +0200

    add t-score algorithm

--- corpus_browser/query_processor/tscore.py
+++ corpus_browser/query_processor/tscore.py
@@ -2,8 +2,34 @@
 # -*- Coding:UTF-8 -*-
 
 from query_processor_strategy import QueryProcessorStrategy
+from math import sqrt
 
 
 class TTest(QueryProcessorStrategy):
-    def calc(self):
-        pass
+    '''
+         2.576 is the critical value for deviation = 005.
+         So we cannot reject the null hypothesis if it less than it.
+    '''
+    def calc(self, freq1, freq2, collocate_freq, corpus_size, span=6):
+        self.freq1, self.freq2 = float(freq1), float(freq2)
+        self.collocate_freq = float(collocate_freq)
+        self.corpus_size = float(corpus_size)
+        self.span = span
+
+        return (self.sample_mean - self.mean ) /             \
+               sqrt(self.sample_variance/self.corpus_size)
+
+    def posipility(self, freq):
+        return freq / float(self.corpus_size)
+
+    @property
+    def mean(self):
+        return self.posipility(self.freq1) * self.posipility(self.freq2)
+
+    @property
+    def sample_mean(self):
+        return self.posipility(self.collocate_freq)
+
+    @property
+    def sample_variance(self):
+        return self.sample_mean
