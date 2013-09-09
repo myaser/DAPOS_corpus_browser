0aa696690a819198da290c66ea7a9420a128ed3c
myaser <me.MahmoudYaser@gmail.com>
2013-09-09 21:29:57 +0200

    ngrams partial

--- corpus_browser/query_processor/ngram.py
+++ corpus_browser/query_processor/ngram.py
@@ -1,15 +1,24 @@
-from query_processor import Operator
-from nltk.model.ngram import NgramModel
 
 
-class NGram(Operator):
+class NGram():
 
-    def __init__(self, n, estimator=None, *estimator_args, **estimator_kwargs):
-        pass
+    def __init__(self, n, index, estimator=None):
 
-    def operate(self, queryset):
-        '''
-        calculate the propability of phrase (from queryset)
-        using language_model, estimate_method from initializer
-        '''
-        pass
+        self._k = n - 1
+        self.index = index
+        estimator.set_index(index)
+        self.estimator = estimator
+
+    def prob(self, statement):
+        result = 1
+        for i in range(len(statement) - 1, -1, -1):
+            token = statement[i]
+            if i < self._k:
+                # heading empty strings
+#                 history = [''] * (self._k - i) + statement[:i]
+                history = statement[:i]
+            else:
+                history = statement[i - self._k: i]
+            result *= self.estimator(token=token,
+                                     history=history)
+        return result
--- corpus_browser/query_processor/probability.py
+++ corpus_browser/query_processor/probability.py
@@ -1,127 +1,45 @@
-from nltk.probability import FreqDist
 
 
-class IndexFreqDist(FreqDist):
+class Estimator():
+    # TODO: avoid underflow
 
-    def __init__(self, index=None):
+    def __call__(self, token=None, history=None):
+        raise NotImplementedError()
+
+    def set_index(self, index):
         self.index = index
 
-    def __getitem__(self, sample):
-        if isinstance(sample, tuple):
-            return len(self.index.consequent(token__in=sample))  # it will work with ngrams but won't with collocations!
-        else:
-            return self.index.objects.get(token=sample).term_frequency or 0
 
-    def freq(self, sample):
-        N = self.N()
-        if N < 1:
-            return 0
-        return float(self[sample]) / N
+class MLEEstimator(Estimator):  # maximum likelihood
+    def __call__(self, token=None, history=None):
+        # note: you are counting documents not occurance in documents
+        count_history = len(self.index.objects.consequent(token__in=history))
+        joint_count = len(self.index.objects.consequent(
+                                            token__in=history.append(token)))
+        return joint_count / float(count_history)
 
-    def max(self):
-        max_token = self.index.objects.order_by("-term_frequency").first()
-        return (max_token.token, max_token.term_frequency)
 
-    def inc(self, sample, count=1):
+class LidstoneEstimator(Estimator):  # add k smoothing
+    pass
+
+
+class LaplaceEstimator(LidstoneEstimator):  # add one smoothing k=1
+    pass
+
+
+class ELEEstimator(LidstoneEstimator):  # expected likelihood k=0.5
+    pass
+
+
+class WittenBellEstimator(Estimator):  # wittenbell estimate
+    pass
+
+
+class GoodTuringEstimator(Estimator):
+    def __init__(self):
         raise NotImplementedError()
 
-    def __setitem__(self, sample, value):
+
+class SimpleGoodTuringEstimator(Estimator):
+    def __init__(self):
         raise NotImplementedError()
-
-    def N(self):
-        return sum(map(lambda term: term.term_frequency, self.index.objects))
-
-    def B(self):
-        return self.index.objects.count()
-
-    def samples(self):
-        raise NotImplementedError()
-
-    def hapaxes(self):
-        return self.Nr(1)
-
-    def Nr(self, r, bins=None):
-        if r < 0: raise IndexError, 'FreqDist.Nr(): r must be non-negative'
-
-        # Special case for Nr(0):
-        if r == 0:
-            if bins is None: return 0
-            else: return bins-self.B()
-
-        return self.index.objects.get(term_frequency=r).count()
-
-    def _cache_Nr_values(self):
-        raise NotImplementedError()
-
-    def _cumulative_frequencies(self, samples=None):
-        raise NotImplementedError()
-
-    def plot(self, *args, **kwargs):
-        raise NotImplementedError()
-
-    def tabulate(self, *args, **kwargs):
-        raise NotImplementedError()
-
-    def _sort_keys_by_value(self):
-        raise NotImplementedError()
-
-    def keys(self):
-        raise NotImplementedError()
-
-    def values(self):
-        raise NotImplementedError()
-
-    def items(self):
-        raise NotImplementedError()
-
-    def __iter__(self):
-        raise NotImplementedError()
-
-    def iterkeys(self):
-        raise NotImplementedError()
-
-    def itervalues(self):
-        raise NotImplementedError()
-
-    def iteritems(self):
-        raise NotImplementedError()
-
-    def copy(self):
-        raise NotImplementedError()
-
-    def update(self, samples):
-        raise NotImplementedError()
-
-    def pop(self, other):
-        raise NotImplementedError()
-
-    def popitem(self):
-        raise NotImplementedError()
-
-    def clear(self):
-        raise NotImplementedError()
-
-    def _reset_caches(self):
-        raise NotImplementedError()
-
-    def __add__(self, other):
-        raise NotImplementedError()
-
-    def __le__(self, other):
-        raise NotImplementedError()
-
-    def __lt__(self, other):
-        raise NotImplementedError()
-
-    def __ge__(self, other):
-        raise NotImplementedError()
-
-    def __gt__(self, other):
-        raise NotImplementedError()
-
-    def __repr__(self):
-        raise NotImplementedError()
-
-    def __str__(self):
-        raise NotImplementedError()
-
