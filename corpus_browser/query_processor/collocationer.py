3563cd5962f8087764805fa64c1f6043c06809ab
moemen <moemenology@gmail.com>
2013-09-13 08:03:23 +0200

    test collocationer

--- corpus_browser/query_processor/collocationer.py
+++ corpus_browser/query_processor/collocationer.py
@@ -12,17 +12,19 @@ class Collocationer(Operator):
 
     def __init__(self, scoring_algorithm, window=5):
         self.scoring_algorithm = scoring_algorithm
+        self.scoring_fn = self.supported_scoring_algorithms.get(
+                                                    self.scoring_algorithm)
         self.window = window
 
-    def _other_tokens(self, queryset, window):
+    def _other_tokens(self, queryset, tokens, window):
         documents = map(lambda posing: posing.document, queryset)
         other_tokens = set([])
         for document in documents:
-            other_tokens = other_tokens | (set(document.tokens) - self.tokens)
+            other_tokens = other_tokens | (set(document.tokens) - tokens)
         return other_tokens
 
-    def extract_freq(self, w1, w2):
-        freq1 = len(self.queryset)
+    def extract_freq(self, w1, w2, queryset):
+        freq1 = len(queryset)
         freq2 = self.index.objects.frequency(w2)
         collocation_freq = self.index.objects.frequency(w1, w2, window=self.window)
 
@@ -34,17 +36,16 @@ class Collocationer(Operator):
                     (self.tokens, token),
                         self.scoring_fn(*self.extract_freq(
                         self.tokens,
-                        token)
+                        token,
+                        self.queryset)
                     )
                 ) for token in self.other_tokens
             ]
 
     def operate(self, queryset, tokens):
         self.queryset = queryset
-        self.scoring_fn = self.supported_scoring_algorithms.get(
-                                                    self.scoring_algorithm)
         self.tokens = set(tokens)
-        self.other_tokens = self._other_tokens(self.queryset, self.window)
+        self.other_tokens = self._other_tokens(self.queryset, self.tokens, self.window)
 
         return sorted(self.collocations(self.tokens, self.other_tokens),
                       reverse=True, key=lambda item: item[1])
--- corpus_browser/query_processor/tests/test_collocationer.py
+++ corpus_browser/query_processor/tests/test_collocationer.py
@@ -1,22 +1,57 @@
+# -*- coding: UTF-8 -*-
+
+import os
+from corpus_browser.settings import PROJECT_ROOT
 from utils.tests import MongoTestCase
+from indexer.models import AuxiliaryIndex, Tweet
+from query_processor.collocationer import Collocationer
+from query_processor import QueryProcessor
+from collections import Iterable
 
 
 class TestCollocationer(MongoTestCase):
 
-    def test__other_tokens(self):
-        pass
+    def setUp(self):
+        fixture_file = os.path.join(PROJECT_ROOT,
+                                            'indexer/fixtures/testindex.json')
+        fixture = open(fixture_file).read()
+        AuxiliaryIndex.load_data(fixture)
+
+        fixture_file = os.path.join(PROJECT_ROOT,
+                                            'indexer/fixtures/testtweets.json')
+        tweet_fixture = open(fixture_file).read()
+        Tweet.load_data(tweet_fixture)
+
+        self.collocationer = Collocationer('t-score')
+        self.collocationer.set_index(AuxiliaryIndex)
+        self.tokens = [u'الله', u'كبير']
+        self.queryset = AuxiliaryIndex.objects.intersect(token__in=self.tokens)
+
+    def test_other_tokens(self):
+        tokens = self.collocationer._other_tokens(self.queryset, set(self.tokens), 5)
+        self.assertEqual(set([
+            u'الفاشلين', u'في', u'الخروج', u'الليلة', u'دي', u'زيي', u'اقعدوا',
+            u'صلوا', u'وادعوا..', u'الدعا', u'بيرد', u'القضا،', u'ادعو', u'خوف',
+            u'وطمع..', u'الفاشلين', u'في', u'الخروج',
+            u'الليلة', u'دي', u'زيي', u'اقعدوا', u'صلوا', u'وادعوا..', u'الدعا',
+            u'بيرد', u'القضا،', u'ادعو', u'خوف', u'وطمع..']),
+            tokens)
 
     def test_extract_freq(self):
-        pass
-
-    def collocations(self):
-        pass
+        freqs = self.collocationer.extract_freq(self.tokens, u'خوف', self.queryset)
+        self.assertEqual((1, (1, 1), 7222.0), freqs)
 
     def test_t_score(self):
-        pass
+        query_processor = QueryProcessor(AuxiliaryIndex, u'الله كبير', Collocationer('t-score'))
+        result = query_processor.excute_query()
+        self.assertTrue(isinstance(result, Iterable))
 
     def test_mutual_information(self):
-        pass
+        query_processor = QueryProcessor(AuxiliaryIndex, u'الله كبير', Collocationer('mutual_information'))
+        result = query_processor.excute_query()
+        self.assertTrue(isinstance(result, Iterable))
 
     def test_log_likelihood(self):
-        pass
+        query_processor = QueryProcessor(AuxiliaryIndex, u'الله كبير', Collocationer('log_likelihood'))
+        result = query_processor.excute_query()
+        self.assertTrue(isinstance(result, Iterable))
