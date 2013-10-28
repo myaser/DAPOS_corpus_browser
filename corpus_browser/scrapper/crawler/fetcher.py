be4027339fdece16689e83310729852e6c00fe0a
moemen <moemenology@gmail.com>
2013-10-28 13:19:58 +0200

    check since_id to ensure the first tweets fetch

--- corpus_browser/scrapper/crawler/fetcher.py
+++ corpus_browser/scrapper/crawler/fetcher.py
@@ -5,17 +5,29 @@ def fetch_hash_tweets(hashtag, last_tweet=None, **params):
     if not hashtag.startswith('#'):
         hashtag = '#' + hashtag
     print u"searching for {0} ...".format(hashtag)
-    return settings.twitter.search(
-        q=hashtag,
-        since_id=last_tweet,
-        **params
-    )['statuses']
+    if last_tweet is not None:
+        return settings.twitter.search(
+            q=hashtag,
+            since_id=last_tweet,
+            **params
+        )['statuses']
+    else:
+        return settings.twitter.search(
+            q=hashtag,
+            **params
+        )['statuses']
 
 
 def fetch_user_tweets(username, last_tweet=None, **params):
     print u'searching for @{0}'.format(username)
-    return settings.twitter.get_home_timeline(
-        screen_name=username,
-        since_id=last_tweet,
-        **params
-    )
+    if last_tweet is not None:
+        return settings.twitter.get_home_timeline(
+            screen_name=username,
+            since_id=last_tweet,
+            **params
+        )
+    else:
+        return settings.twitter.get_home_timeline(
+            screen_name=username,
+            **params
+        )
